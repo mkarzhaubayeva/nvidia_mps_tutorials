e4958539ca60b6a83511eb9fa35568ee0b14ddcc
Meruyert Karzhaubayeva <71264854+mkarzhaubayeva@users.noreply.github.com>
2021-11-01 12:35:50 +0500

    Add files via upload

---
+++ vectorsAdd/vectorAdd.cu
@@ -0,0 +1,40 @@
+//to parallelize execution:
+//1) make the variables accessible to the GPU and CPU;
+//2) specify the launch config of kernel;
+//3) parallelize the kernel function to be run on GPU.
+
+
+#include <stdio.h>
+#define SIZE	1024
+
+__global__ void VectorAdd(int *a, int *b, int *c, int n) {
+	int i = threadIdx.x;
+	if (i < n)
+		c[i] = a[i] + b[i];
+}
+
+int main() {
+	int *a, *b, *c;
+
+	cudaMallocManaged(&a, SIZE * sizeof(int));
+	cudaMallocManaged(&b, SIZE * sizeof(int));
+	cudaMallocManaged(&c, SIZE * sizeof(int));
+
+	for (int i = 0; i < SIZE; i++) {
+		a[i] = i;
+		b[i] = i;
+		c[i] = 0;
+	}
+
+	VectorAdd <<<1, SIZE>>> (a, b, c, SIZE);
+	cudaDeviceSynchronize();
+
+	for (int i = 0; i < 5; i++)
+		printf("c[%d] = %d\n", i, c[i]);
+
+	cudaFree(a);
+	cudaFree(b);
+	cudaFree(c);
+
+	return 0;
+}
